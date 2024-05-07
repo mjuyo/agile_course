3e09f52977f15a3768f30e45a96d97a2a19e83e3
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-05-07 15:07:24 -0500

    Added challenge00.rb with all exercises coded

---
+++ challenge00.rb
@@ -0,0 +1,37 @@
+# Challenge 00
+# Marco Juyo
+
+# Asking for the input
+puts "Enter the amount: "
+
+# Variable and methods for the input
+amount = gets.chomp
+
+# Assigning sub_total and converting it to float with to_f
+sub_total = amount.to_f
+
+# Constants rates
+PST = 0.07
+GST = 0.05
+
+# Taxes values
+pst_value = PST * sub_total
+gst_value = GST * sub_total
+
+# Grand Total
+grand_total = sub_total + pst_value + gst_value
+
+# Outputs
+puts "Subtotal: $#{sub_total}"
+puts "PST: $#{pst_rate} - #{PST * 100}%"
+puts "GST: $#{gst_rate} - #{GST * 100}%"
+puts "Grand Total: $#{grand_total}"
+
+# Conditional messages
+if grand_total <= 5
+    puts "Pocket Change"
+elsif grand_total < 20
+    puts "Wallet Time"
+else
+    puts "Charge It"
+end
---
+++ hello.rb
@@ -0,0 +1,24 @@
+pp "Hello World!"
+
+# This is a comment
+=begin
+jdsfsfd
+fdff
+fgd
+g
+dgdgdg
+=end
+
+def say_goodnight(name)
+    puts "Goodnight, #{name}."
+end
+
+say_goodnight ("Marco")
+
+4.times { puts 'Forty two' }
+
+4.times do 
+    puts 'Do end blocks'
+end
+
+pp puts
